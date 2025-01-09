01a34ffa4e8eb76d037dd3b028a4e382fe7cb058
giolga <giolga.kumi@gmail.com>
2025-01-09 15:51:18 +0400

    Add IUserService

---
+++ DotnetAuth/Service/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using DotnetAuth.Domain.Contracts;
+
+namespace DotnetAuth.Service
+{
+    public interface IUserService
+    {
+        Task<UserResponse> RegisterAsync(UserRegisterRequest request);
+        Task<CurrentUserResponse> GetCurrentUserAsync();
+        Task<UserResponse> GetUserByIdAsync(Guid id);
+        Task<UserResponse> UpdateAsync(Guid id, UpdateUserRequest request);
+        Task DeleteAsync(Guid id);
+        Task<RevokeRefreshToken> RevokeRefreshToken(RefreshTokenRequest refreshTokenRequest);
+        Task<CurrentUserResponse> RefreshTokenAsync(RefreshTokenRequest request);
+        Task<UserResponse> LoginAsync(UserLoginRequest request);
+    }
+}
